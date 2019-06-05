18f801f1629e7d1aa9b1033186fa00f024649789
13638680321@163.com <13638680321@163.com>
2019-06-05 10:22:17 +0800

    learn

---
+++ learn-flink/src/main/java/com/hqbhoho/bigdata/learnFlink/table_sql/concepts/stream/DetectingPatternsInTablesExample.java
@@ -0,0 +1,11 @@
+package com.hqbhoho.bigdata.learnFlink.table_sql.concepts.stream;
+
+/**
+ * describe:
+ *
+ * @author hqbhoho
+ * @version [v1.0]
+ * @date 2019/06/05
+ */
+public class DetectingPatternsInTablesExample {
+}
---
+++ learn-flink/src/main/java/com/hqbhoho/bigdata/learnFlink/table_sql/sql/PatternRecognitionExample.java
@@ -0,0 +1,13 @@
+package com.hqbhoho.bigdata.learnFlink.table_sql.sql;
+
+/**
+ * describe:
+ *
+ * {@link com.hqbhoho.bigdata.learnFlink.table_sql.concepts.stream.DetectingPatternsInTablesExample}
+ *
+ * @author hqbhoho
+ * @version [v1.0]
+ * @date 2019/06/05
+ */
+public class PatternRecognitionExample {
+}
