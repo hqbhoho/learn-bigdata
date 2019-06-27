c7b980a8415969811dca0f58a6d3d31d800f8b8d
hqbhoho <13638680321@163.com>
2019-06-27 10:36:37 +0800

    learn

--- learn-flink/src/main/java/com/hqbhoho/bigdata/learnFlink/batch/iteration/DeltaIterateOperatorExample.java
+++ learn-flink/src/main/java/com/hqbhoho/bigdata/learnFlink/batch/iteration/DeltaIterateOperatorExample.java
@@ -1,17 +1,82 @@
 package com.hqbhoho.bigdata.learnFlink.batch.iteration;
 
+import org.apache.flink.api.common.functions.FlatJoinFunction;
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DeltaIteration;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.util.Collector;
+
+import java.util.Optional;
+
 /**
  * describe:
  * 增量迭代   以官网Propagate Minimum in Graph为例进行说明
  * refence: https://ci.apache.org/projects/flink/flink-docs-release-1.8/dev/batch/iterations.html
- *
+ * 顶点之间的关系
+ * 1<===>2   2<===>3   2<===>4   3<===>4   5<===>6   5<===>7   6<===>7
+ * 迭代结束条件
+ * The default termination condition for delta iterations is specified by the empty workset convergence criterion and a maximum number of iterations.
+ * The iteration will terminate when a produced next workset is empty or when the maximum number of iterations is reached
+ * 1. 达到最大迭代次数
+ * 2. new workset 为空
+ * 3. 自定义收敛条件
  *
  * @author hqbhoho
  * @version [v1.0]
  * @date 2019/06/27
  */
 public class DeltaIterateOperatorExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // 获取执行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        // init SolutionSet and workset
+        DataSet<Tuple2<Long, Long>> initialSolutionSet = InitDeltalterateData.initialSolutionSet(env);
+        // 获取节点之间的连接关系
+        DataSet<Tuple2<Long, Long>> initialEdges = InitDeltalterateData.initialEdges(env);
+        //  create a DeltaIteration
+        //在initialSolutionSet上调用iterateDelta（initialWorkset，maxIterations，key）
+        DeltaIteration<Tuple2<Long, Long>, Tuple2<Long, Long>> it =
+                initialSolutionSet.iterateDelta(initialSolutionSet, 10, 0);
+        /**
+         * 以顶点2为例，说明此处的迭代逻辑
+         * 初始的workest SolutionSet 均为   (2,2)
+         * edges: (2,1) (2,3) (2,4)
+         * 第一次join(workest,edges) --> (2,2,1) (2,2,3) (2,2,4)
+         * 第二次join(第一次join结果,SolutionSet)  --->  (2,2,1)(1,1)    (2,2,3)(3,3)     (2,2,4)(4,4)
+         * if (second.f1 < first.f1) {
+         *     out.collect(Tuple2.of(first.f0, second.f1));
+         * }
+         * 此处就会输出 (2,1)
+         * 此处算法逻辑还不是很完善      仅供参考
+         */
+        DataSet<Tuple2<Long, Long>> changes = it.getWorkset()
+                .join(initialEdges)
+                .where(0)
+                .equalTo(0)
+                .with(new JoinFunction<Tuple2<Long, Long>, Tuple2<Long, Long>, Tuple3<Long, Long, Long>>() {
+                    @Override
+                    public Tuple3<Long, Long, Long> join(Tuple2<Long, Long> first, Tuple2<Long, Long> second) throws Exception {
+                        return Tuple3.of(first.f0, first.f1, second.f1);
+                    }
+                })
+                .join(it.getSolutionSet())
+                .where(2)
+                .equalTo(0)
+                .with(new FlatJoinFunction<Tuple3<Long, Long, Long>, Tuple2<Long, Long>, Tuple2<Long, Long>>() {
+                    @Override
+                    public void join(Tuple3<Long, Long, Long> first, Tuple2<Long, Long> second, Collector<Tuple2<Long, Long>> out) throws Exception {
+                        Optional.ofNullable("process event: " + first).ifPresent(System.out::println);
+                        if (second.f1 < first.f1) {
 
+                            out.collect(Tuple2.of(first.f0, second.f1));
+                        }
+                    }
+                });
+        // detlaSolutionSet  and   new workset
+        DataSet<Tuple2<Long, Long>> result = it.closeWith(changes, changes);
+        result.print();
     }
 }
--- learn-flink/src/main/java/com/hqbhoho/bigdata/learnFlink/batch/iteration/IterateOperatorExample.java
+++ learn-flink/src/main/java/com/hqbhoho/bigdata/learnFlink/batch/iteration/IterateOperatorExample.java
@@ -12,14 +12,17 @@ import java.util.Optional;
 /**
  * describe:
  * 全量迭代
- *
+ * <p>
+ * 迭代结束条件：
+ * 1. 达到最大迭代次数
+ * 2. 自定义收敛条件
  *
  * @author hqbhoho
  * @version [v1.0]
  * @date 2019/06/27
  */
 public class IterateOperatorExample {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         // 获取执行环境
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         // 模拟数据源
@@ -37,11 +40,11 @@ public class IterateOperatorExample {
             @Override
             public String map(String value) throws Exception {
                 Optional.ofNullable("<<<>>>process event: " + value).ifPresent(System.out::println);
-                if(value.contains("a")) value = value.replaceFirst("a","@") ;
+                if (value.contains("a")) value = value.replaceFirst("a", "@");
                 return value;
             }
         });
         DataSet<String> result = initial.closeWith(it);
-        result.map(v -> "result: "+v).print();
+        result.map(v -> "result: " + v).print();
     }
 }
